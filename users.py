c8520b484cc5f9415a1fa95e9f5a8ab8ab551a7e
KeithLion <nortonkiara6@gmail.com>
2022-01-15 08:46:22 -0800

    added class method get all which will show all data

--- users.py
+++ users.py
@@ -9,3 +9,12 @@ class User:
         self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+
+    @classmethod
+    def get_all(cls):
+        query = 'select * from users;'
+        results = connectToMySQL('new_schema1').query_db(query)
+        users = []
+        for user in results:
+            users.append(cls(user))
+        return users
