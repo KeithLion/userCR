e760c53ef9bae0855043c55d3c4bc06ab7eaeb71
KeithLion <nortonkiara6@gmail.com>
2022-01-15 12:52:18 -0800

    created routes to display input form and post method and the read all page

--- server.py
+++ server.py
@@ -3,5 +3,26 @@ from users import User
 app = Flask(__name__)
 
 
+@app.route('/')
+def create_user():
+    return render_template('form1.html')
+
+
+@app.route('/create_users', methods=['post'])
+def new_user():
+    data = {
+        'fname': request.form['fname'],
+        'lname': request.form['lname'],
+        'ema': request.form['ema']
+    }
+    User.save(data)
+    return redirect('/allusers')
+
+
+@app.route('/allusers')
+def all():
+    return render_template('form2', users=User.get_all())
+
+
 if __name__ == "__main__":
     app.run(debug=True)
